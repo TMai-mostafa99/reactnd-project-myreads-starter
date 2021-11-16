5590d3463821e33835eb562a6eaf6fa569f3dc38
unknown <maimostafaibrahim@gmail.com>
2021-11-16 02:41:31 +0200

    fix update

--- src/BookShelfChanger.js
+++ src/BookShelfChanger.js
@@ -1,24 +1,26 @@
 import React, { Component } from 'react'
 
 class BookShelfChanger extends Component {
-   render(){
-      const { books,book , changeBookShelf } = this.props;
+
   
-      let currentShelf = 'none';
+   render(){
 
-    // if book is in current list, set current shelf to book.shelf
-
-    for (let i of books) {
-      if (i.id === book.id) { currentShelf = i.shelf;
-        break;
-      }
-    }
     
-    //console.log("the ",book.title,"has shelf of ", book.shelf)
+      const { books,book , changeBookShelf } = this.props;
+     
+      let currentShelf = 'none'
+      const foundBook = books.find( (i) => (i.id === book.id)); //will return object
+
+      if(foundBook !== undefined)
+       currentShelf = foundBook.shelf
+        else
+       currentShelf='none'
+    
+     console.log("title: ",book.title,"shelf: ",book.shelf , "current shelf: ", currentShelf)
        
         return(
             <div className="book-shelf-changer">
-            <select defaultValue={ currentShelf }  onChange={(event) => changeBookShelf(book ,event.target.value)} >
+            <select value={ currentShelf}  onChange={(event) => changeBookShelf(book ,event.target.value)} >
               <option  value="none" disabled >Move to...</option>
               <option  value="currentlyReading">Currently Reading</option>
               <option  value="wantToRead">Want to Read</option>
